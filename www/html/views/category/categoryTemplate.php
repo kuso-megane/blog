4159f165cc69b827e0e74f8df46bd53cd327a6a5
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-02-09 07:39:32 +0900

    views/categoryTemplate.php article-box を繰り返し処理に

--- www/html/views/category/categoryTemplate.php
+++ www/html/views/category/categoryTemplate.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" type="text/css" href=<?php echo $styleSheetPath. "category/category.css"; ?>>
   </head>
   <body>
-    <?php require '/parts/header.php';?>
+    <?php require 'templateparts/header.php';?>
 
     <div id="main">
       <div id="main--left">
@@ -20,36 +20,30 @@
           top &gt; xxx &gt; yyy
         </div>
         <div id="main--title">
-          <p></p>
+          <p>最近の投稿</p>
         </div>
         <div id="page-switch">
           <p id="page-switch--previous"><a href="">前の10件</a> </p>
           <p id="page-switch--next"><a href="">次の10件</a></p>
         </div>
 
-        <div class="article-box">
-          <div class="article-thumbnail-container">
-            <img src=<?php echo ($imgPath."test2.jpg"); ?> alt="テスト画像" class="article-thumbnail">
-          </div>
-          <div class="article-main">
-            <p class="article-update-date">2020-8-13</p>
-            <p class="article-title"><a href="xxx">テスト2</a></p>
-          </div>
-        </div>
+        <?php for ($i = 0; $i < 10; ++$i): ?>
 
         <div class="article-box">
-          <div class="article-thumbnail-container">
-            <img src=<?php echo ($imgPath."test2.jpg"); ?> alt="テスト画像" class="article-thumbnail">
-          </div>
-          <div class="article-main">
-            <p class="article-update-date">2020-8-13</p>
-            <p class="article-title"><a href="xxx">テスト1</a></p>
-          </div>
+            <div class="article-thumbnail-container">
+                <img src=<?php echo ($imgPath."test2.jpg"); ?> alt="テスト画像" class="article-thumbnail">
+            </div>
+            <div class="article-main">
+                <p class="article-update-date">2020-8-13</p>
+                <p class="article-title"><a href="xxx">テスト2</a></p>
+            </div>
         </div>
+        
+        <?php endfor;?>
           
       </div>
       <!--mainの右側に置く諸々-->
-      <?php require 'parts/main-right.php';?>
+      <?php require 'templateparts/main--sidebar.php';?>
     </div>
     <div id="footer">
       
