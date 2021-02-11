184e1c3fc468fb881c99a0311d6340ecf7e9d038
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-02-12 07:39:20 +0900

    views/components/main--side 検索フォーム仮実装

--- www/html/views/components/main--sidebar.php
+++ www/html/views/components/main--sidebar.php
@@ -1,31 +1,24 @@
-<?php
-/*
-use myapp\viewModel\templateParts\MainSidebarVM;
 
-$vm = (new MainSidebarVM())->getDataFromModel();
+<div id="main--sidebar">
+    <div id="search-container">
+        <div id="search-box"><input type="search" placeholder="フリーワード検索"></div>
+        <p>→詳細検索は<a href="">コチラ</a></p>
+    </div>
+    <div id="self-intro-box">
+        <p>自己紹介</p>
+        <p>→紹介記事は<a href="">コチラ</a></p>
+        <p>twitter垢 <a href="https://twitter.com/kusomeg61908444">@kusomeg61908444</a></p>
+    </div>
+    <div id="category-list-container">
+        <p>カテゴリ一覧</p>
+        <ul>
+            <?php foreach($vm->getCategoryArtclCount() as $category => $count): ?>
 
-$artclNumOnCategory = $vm->getArtclNumOnCategory();
-$artclNumOnSubCategory = $vm->getArtclNumOnSubCategory();
-*/
-?>
-
-<div id="search-container">
-    <div id="search-box"></div>
-    <p><a href="">詳細検索はコチラ</a></p>
-</div>
-<div id="self-intro-box">
-    <p>自己紹介</p>
-    <a href=""><p>→紹介記事</p></a>
-</div>
-<div id="category-list-container">
-    <p>カテゴリ一覧</p>
-    <ul>
-        <?php foreach($vm->getCategoryArtclCount() as $category => $count): ?>
-
-        <li><a href=""><?php echo "{$category}({$count})"; ?></a></li>
-        
-        <?php endforeach; ?>
-    </ul>
+            <li><a href=""><?php echo "{$category}({$count})"; ?></a></li>
+            
+            <?php endforeach; ?>
+        </ul>
+    </div>
 </div>
 
 <!--
