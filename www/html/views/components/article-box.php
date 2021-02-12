7e91cd0d92393b9ed56d683d2c6e4c1189bce713
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-02-13 04:03:56 +0900

    article-box 文字折り返し

--- www/html/views/components/article-box.php
+++ www/html/views/components/article-box.php
@@ -1,13 +1,15 @@
 <?php foreach ($vm->getRecentArtclInfos() as $artcls): ?>
 
 <div class="article-box">
-    <a class="article-thumbnail-container linkbox" href="/xxx">
-        <img src=<?php echo ($imgUrl."test2.jpg"); ?> alt="テスト画像" class="article-thumbnail">
-    </a>
-    <a class="article-main linkbox" href="/xxx">
-        <p class="article-update-date"><?php echo $artcls['updateDate']; ?></p>
-        <p class="article-title"><?php echo $artcls['title']; ?></p>
-    </a>
+    <a href="/xxx" class="linkbox">
+        <div class="article-thumbnail-container">
+            <img src=<?php echo ($imgUrl."test2.jpg"); ?> alt="テスト画像" class="article-thumbnail">
+        </div>
+        <div class="article-main">
+            <p class="article-update-date"><?php echo $artcls['updateDate']; ?></p>
+            <p class="article-title break-word"><?php echo $artcls['title']; ?></p>
+        </div>
+    </a>      
 </div>
 
 <?php endforeach;?>
