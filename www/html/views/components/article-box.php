ef0f46f6eb25c256685aa1891893a1f696b8de0b
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-02-13 03:30:10 +0900

    views/components/article-box   リンクを記事ボックス全体に拡大

--- www/html/views/components/article-box.php
+++ www/html/views/components/article-box.php
@@ -1,13 +1,13 @@
 <?php foreach ($vm->getRecentArtclInfos() as $artcls): ?>
 
 <div class="article-box">
-    <div class="article-thumbnail-container">
+    <a class="article-thumbnail-container linkbox" href="/xxx">
         <img src=<?php echo ($imgUrl."test2.jpg"); ?> alt="テスト画像" class="article-thumbnail">
-    </div>
-    <div class="article-main">
+    </a>
+    <a class="article-main linkbox" href="/xxx">
         <p class="article-update-date"><?php echo $artcls['updateDate']; ?></p>
-        <p class="article-title"><a href="xxx"><?php echo $artcls['title']; ?></a></p>
-    </div>
+        <p class="article-title"><?php echo $artcls['title']; ?></p>
+    </a>
 </div>
 
 <?php endforeach;?>
