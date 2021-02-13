908cd3cad2e9a31322827dd687d4a92618e85935
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-02-13 11:01:47 +0900

    html/ main--sidebar 修正、twitter, profile追加

--- www/html/views/components/main--sidebar.php
+++ www/html/views/components/main--sidebar.php
@@ -3,14 +3,25 @@
     <div id="search-container">
 
         <form action="/search" method="get">
-            <input id="search-box" type="search" name="artclName" placeholder="フリーワード検索">
+            <input id="search-box" type="search" name="artclName" placeholder="記事フリーワード検索">
         </form>
         <p>→詳細検索は<a href="">コチラ</a></p>
     </div>
-    <div id="self-intro-box">
-        <p>自己紹介</p>
-        <p>→紹介記事は<a href="">コチラ</a></p>
-        <p>twitter垢→ <a href="https://twitter.com/kusomeg61908444">@kusomeg61908444</a></p>
+    <div id="my-profile">
+        
+        <div id="my-profile--img-container">
+            <img class="img-to-circle" src="/asset/img/myProfile.jpg" alt="プロフィール画像">
+        </div>
+        <h4 id="my-profile--name">クソメガネ</h2>
+        <p id="my-profile--txt" class="break-word">
+            &emsp;ゲーム開発業界志望の文系大学生。怠惰な生活を好む、クソなメガネ。たまに、めっちゃ頑張る。
+        </p>
+        
+        <!--twitter follow button-->
+        <a id="twitter-follow-button" href="https://twitter.com/kusomeg61908444?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">
+            Follow @kusomeg61908444
+        </a>
+        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
     </div>
     <div id="category-list-container">
         <p>カテゴリ一覧</p>
