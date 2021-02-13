d4634ce6cab2942c96ff3dcc1af0434e0e31dbe1
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-02-13 11:14:00 +0900

    fix: views/category/index 終了タグ修正

--- www/html/views/category/index.php
+++ www/html/views/category/index.php
@@ -4,7 +4,7 @@
         <p><a href="/index">top</a></p>
     </div>
     <div id="main--title">
-        <p id="main-title">最近の投稿</>
+        <p id="main-title">最近の投稿</p>
     </div>
     <div id="page-switch">
         <p id="page-switch--previous"><a href="">前の10件</a> </p>
