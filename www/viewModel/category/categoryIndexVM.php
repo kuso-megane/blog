e7f1e88e8af7c187ddba96fa1d459503ccf45c60
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-02-20 20:43:56 +0900

    viewModel mainSidebarVMをtraitとして実装

--- www/viewModel/category/categoryIndexVM.php
+++ www/viewModel/category/categoryIndexVM.php
@@ -3,36 +3,30 @@
 namespace myapp\viewModel\category;
 
 use myapp\viewModel\viewModelInterface;
+use myapp\ViewModel\components\mainSidebarVM;
 
 class CategoryIndexVM implements viewModelInterface
 {
 
     /*
         $recentArtclInfos = [$artclInfo, ...], 
-    $artclInfo = ['artclId' => int, 'title' => string, 'updateDate' => date, 'thumbnailImg' => ?]
+        $artclInfo = ['artclId' => int, 'title' => string, 'updateDate' => date, 'thumbnailImg' => ?]
     */
     private $recentArtclInfos; 
 
-    //for main--sidebar component
-    /*
-        (e.g.)
-        $categoryArtclCount = ['プログラミング' => int, '読書' => int, ...] 
-    */
-    private $categoryArtclCount; 
-
-    /*
-        (e.g.)
-        $subCategoryArtclCount = ['プログラミング' => ['web' => int, 'game' => int,...] ,
-                                '読書' => ['マンガ' => int ,'小説' => int]] 
-    */
-    private $subCategoryArtclCount;
+    use mainSidebarVM {
+        mainSidebarVM::__construct as mainSidebarVM__construct;
+    }
+    
 
 
     public function __construct(array $data)
     {
         $this->recentArtclInfos = $data['recentArtclInfos'];
-        $this->categoryArtclCount = $data['categoryArtclCount'];
-        $this->subCategoryArtclCount = $data['subCategoryArtclCount'];
+        $categoryArtclCount = $data['categoryArtclCount'];
+        $subCategoryArtclCount = $data['subCategoryArtclCount'];
+
+        $this->mainSidebarVM__construct($categoryArtclCount, $subCategoryArtclCount);
     }
 
 
@@ -41,14 +35,4 @@ class CategoryIndexVM implements viewModelInterface
         return $this->recentArtclInfos;
     }
 
-    public function getCategoryArtclCount():array
-    {
-        return $this->categoryArtclCount;
-    }
-
-
-    public function getSubCategoryArtclCount():array
-    {
-        return $this->subCategoryArtclCount;
-    }
 }
---
+++ www/viewModel/components/mainSidebarVM.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace myapp\ViewModel\components;
+
+trait mainSidebarVM
+{
+    /*
+        (e.g.)
+        $categoryArtclCount = ['プログラミング' => int, '読書' => int, ...] 
+    */
+    private $categoryArtclCount; 
+
+    /*
+        (e.g.)
+        $subCategoryArtclCount = ['プログラミング' => ['web' => int, 'game' => int,...] ,
+                                '読書' => ['マンガ' => int ,'小説' => int]] 
+    */
+    private $subCategoryArtclCount;
+
+
+    public function __construct(array $categoryArtclCount, array $subCategoryArtclCount)
+    {
+        $this->categoryArtclCount = $categoryArtclCount;
+        $this->subCategoryArtclCount = $subCategoryArtclCount;
+    }
+
+    
+    public function getCategoryArtclCount():array
+    {
+        return $this->categoryArtclCount;
+    }
+
+
+    public function getSubCategoryArtclCount():array
+    {
+        return $this->subCategoryArtclCount;
+    }
+}
