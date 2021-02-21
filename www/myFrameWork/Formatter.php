f9d231a3767087cf51cc5b7571d27386b1d73442
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-02-22 03:23:23 +0900

    fix: myFramework/formatter#objectsToArray()をobjectsArrTo2DArr()に改名

--- www/myFrameWork/Formatter.php
+++ www/myFrameWork/Formatter.php
@@ -5,6 +5,11 @@ namespace myapp\myFrameWork;
 
 class Formatter
 {
+
+    /**
+     * [ob1, ob2] changed into  [ ['id' => 1, 'title' => 'aaa'], ['id' => 2, 'title' => 'bbb'] ] (e.g.)
+     * @param array $objects array of object which has toArray() method
+     */
     public static function objectsToArray(array $objects):array
     {
         foreach($objects as &$object) {
