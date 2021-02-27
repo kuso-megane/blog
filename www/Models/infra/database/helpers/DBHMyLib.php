7c2eeb540b69eb800b536da2deb23236e6881da0
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-02-28 00:15:22 +0900

    fix: DBMyLibをDBHMyLibに改名

---
+++ www/Models/infra/database/helpers/DBHMyLib.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace infra\database\helpers;
+use PDO;
+use PDOException;
+use PDOStatement;
+
+class DBHMyLib
+{
+    private $dbh;
+
+    public function __construct(PDO $dbh)
+    {
+        $this->dbh = $dbh;
+    }
+
+    /**
+     * @param string $tablename
+     * 
+     * @return void
+     */
+    public function truncate(string $tablename):void
+    {
+        $this->dbh->exec('SET foreign_key_checks=0');
+        $this->dbh->exec('TRUNCATE TABLE ' . $tablename);
+        $this->dbh->exec('SET foreign_key_checks=1');     
+    }
+
+
+    /**
+     * @param string $command
+     * 
+     * @return PDOStatement
+     */
+    public function prepare(string $command):PDOStatement
+    {
+        try {
+            $sth = $this->dbh->prepare($command);
+        } catch (PDOException $e) {
+            echo "\nPDO::prepare() failed!\n\t given command: {$command};\n\t {$e->getMessage()}\n";
+        }
+
+        return $sth;
+    }
+
+
+    public function transaction()
+    {
+        
+    }
+}
