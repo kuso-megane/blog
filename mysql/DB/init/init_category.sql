20ebb3a4432137df1f1e6c5c9367e12cc715126a
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-03-02 06:33:27 +0900

    mysql/DB/init csvをsqlファイルに変更

---
+++ mysql/DB/init/init_category.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS Category;
+CREATE TABLE Category;
+
+INSERT INTO Category VALUES(0, 'プログラミング', 0);
+INSERT INTO Category VALUES(0, '考え事', 0);
+INSERT INTO Category VALUES(0, '食事', 0);
+INSERT INTO Category VALUES(0, '読書', 0);
+INSERT INTO Category VALUES(0, 'ゲーム', 0);
+INSERT INTO Category VALUES(0, 'その他', 0);
+
