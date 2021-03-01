d467f55ba60b10cecb5b3713b790aab64e6aa49f
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-03-02 03:51:58 +0900

    fix: main--sidebar myprofileを上に、カテゴリ検索の文字を少し小さく

--- www/html/views/components/main--sidebar.php
+++ www/html/views/components/main--sidebar.php
@@ -5,6 +5,23 @@
 
 
 <div id="main--sidebar">
+    <div id="my-profile">
+        
+        <div id="my-profile--img-container">
+            <img class="img-to-circle" src="/asset/img/myProfile.jpg" alt="プロフィール画像">
+        </div>
+        <h4 id="my-profile--name">クソメガネ</h2>
+        <p id="my-profile--txt" class="break-word">
+            &emsp;ゲーム開発業界志望の文系大学生。怠惰な生活を好む、クソなメガネ。たまに、めっちゃ頑張る。
+        </p>
+        
+        <!--twitter follow button-->
+        <a id="twitter-follow-button" href="https://twitter.com/kusomeg61908444?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">
+            Follow @kusomeg61908444
+        </a>
+        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    </div>
+    
     <div id="search-container">
 
         <form action="/search" method="get">
@@ -41,21 +58,4 @@
             ?>
         </ul>
     </nav>
-
-    <div id="my-profile">
-        
-        <div id="my-profile--img-container">
-            <img class="img-to-circle" src="/asset/img/myProfile.jpg" alt="プロフィール画像">
-        </div>
-        <h4 id="my-profile--name">クソメガネ</h2>
-        <p id="my-profile--txt" class="break-word">
-            &emsp;ゲーム開発業界志望の文系大学生。怠惰な生活を好む、クソなメガネ。たまに、めっちゃ頑張る。
-        </p>
-        
-        <!--twitter follow button-->
-        <a id="twitter-follow-button" href="https://twitter.com/kusomeg61908444?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">
-            Follow @kusomeg61908444
-        </a>
-        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-    </div>
 </div>
