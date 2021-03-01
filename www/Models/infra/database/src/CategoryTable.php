dcc5bba486ef7bd76c331041b02ff482169804fd
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-03-01 23:05:47 +0900

    infra/database/src/CategoryTable#findById()実装

--- www/Models/infra/database/src/CategoryTable.php
+++ www/Models/infra/database/src/CategoryTable.php
@@ -3,12 +3,15 @@
 namespace infra\database\src;
 
 use infra\database\helpers\DBConnection;
+use infra\database\helpers\DBHMyLib;
 use PDO;
+
 
 class CategoryTable
 {
     const TABLENAME = 'Category';
     private $dbh;
+    private $dbhHelper;
 
 
     /**
@@ -17,14 +20,35 @@ class CategoryTable
     public function __construct(bool $is_test=false)
     {
         $this->dbh = (new DBConnection($is_test))->connect(); //PDO
+        $this->dbhHelper = new DBHMyLib($this->dbh);
     }
 
     /**
-     * @return array [['id' => int, 'name' => string, 'num' => int]]
+     * @return array [ 
+     *      ['id' => int, 'name' => string, 'num' => int],
+     *      []
+     *   ]
      */
     public function findAll():array
     {
         $records = $this->dbh->query('SELECT * FROM ' . $this::TABLENAME);
         return $records->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+
+    /**
+     * @param int $id
+     * 
+     * @return array ['id' => int, 'name' => string, 'num' => int]
+     */
+    public function findById($id):array
+    {
+        $command = 'SELECT * FROM ' . $this::TABLENAME . ' WHERE id = :id';
+        $sth = $this->dbhHelper->prepare($command);
+        $sth->execute([':id' => $id]);
+
+        $record = $sth->fetch(PDO::FETCH_ASSOC);
+
+        return $record;
     }
 }
--- www/Models/infra/database/tests/CategoryTableTest.php
+++ www/Models/infra/database/tests/CategoryTableTest.php
@@ -20,22 +20,31 @@ class CategoryTableTest extends TestCase
         $this->dbhHelper = new DBHMyLib($this->dbh);
 
         $this->dbhHelper->truncate($this::TABLENAME);
-    }
 
-
-    public function testFindAll()
-    {
-        $command = 'INSERT INTO ' . $this::TABLENAME . ' VALUES(0, :name, :num)';
-            
+        $command = 'INSERT INTO ' . $this::TABLENAME . ' VALUES(0, :name, :num)';   
         $sth = $this->dbhHelper->prepare($command);
 
         for ($i = 1; $i < 3; ++$i) {
             $sth->execute([':name' => "category{$i}", ':num' => 0]);
         }
+    }
 
+
+    public function testFindAll()
+    {
         $this->assertSame([
             ['id' => 1, 'name' => 'category1', 'num' => 0],
             ['id' => 2, 'name' => 'category2', 'num' => 0]
         ], $this->table->findAll());
+    }
+
+
+    public function testFindById()
+    {
+        $c_id = 1;
+
+        $this->assertSame([
+            'id' => 1, 'name' => 'category1', 'num' => 0
+        ], $this->table->findById($c_id));
     }
 }
