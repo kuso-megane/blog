5a6956dbed67b43b89da736763fff24c679c01d5
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-03-02 07:18:52 +0900

    views/components/main--article-box 記事見つからなかった場合のメッセージ

--- www/html/views/components/main--article-box.php
+++ www/html/views/components/main--article-box.php
@@ -1,19 +1,25 @@
 
 <div id="article-container">
-    <?php foreach($vm['recentArtclInfos'] as $artcls): ?>
+    <?php if($vm['recentArtclInfos'] != NULL): ?>
 
-    <div class="article-box">
-        <a href="/xxx" class="linkbox">
-            <div class="article-thumbnail-container">
-                <img src=<?php echo ($imgUrl."test2.jpg"); ?> alt="テスト画像" class="article-thumbnail">
-            </div>
-            <div class="article-main">
-                <p class="article-update-date"><?php echo $artcls['updateDate']; ?></p>
-                <p class="article-title break-word"><?php echo $artcls['title']; ?></p>
-            </div>
-        </a>      
-    </div>
+        <?php foreach($vm['recentArtclInfos'] as $artcls): ?>
 
-    <?php endforeach;?>
+        <div class="article-box">
+            <a href="/xxx" class="linkbox">
+                <div class="article-thumbnail-container">
+                    <img src=<?php echo ($imgUrl."test2.jpg"); ?> alt="テスト画像" class="article-thumbnail">
+                </div>
+                <div class="article-main">
+                    <p class="article-update-date"><?php echo $artcls['updateDate']; ?></p>
+                    <p class="article-title break-word"><?php echo $artcls['title']; ?></p>
+                </div>
+            </a>      
+        </div>
+
+        <?php endforeach;?>
+
+    <?php else: ?>
+        <p id="no-article-message">該当記事は見つかりませんでした。検索条件を変えてください。</p>
+    <?php endif; ?>
 </div>
     
