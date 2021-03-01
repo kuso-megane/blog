43f279842cfea9620739ad95b144a09de556c773
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-03-02 04:53:28 +0900

    fix; mysql/DB/sampleArticle.sql トランザクション、update Category, SubCategory 追加

--- mysql/DB/sampleArticle.sql
+++ mysql/DB/sampleArticle.sql
@@ -1,1 +1,5 @@
-INSERT INTO Article VALUES(0, 1, 1, "サンプル記事", "default.jpg", "<p>サンプル記事です。</p>", NULL)
+START TRANSACTION;
+INSERT INTO Article VALUES(0, 1, 1, "サンプル記事", "default.jpg", "<p>サンプル記事です。</p>", NULL);
+UPDATE Category SET num = num + 1 WHERE id = 1;
+UPDATE SubCategory SET num = num + 1 WHERE id = 1;
+COMMIT;
