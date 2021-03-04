fcb39f073cd44586627dfd6d57e05840d9678364
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-03-05 05:17:58 +0900

    db 微調整

--- mysql/DB/init/init_category.sql
+++ mysql/DB/init/init_category.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS Category;
 CREATE TABLE Category;
 
-INSERT INTO Category VALUES(0, 'プログラミング', 0);
 INSERT INTO Category VALUES(0, '考え事', 0);
+INSERT INTO Category VALUES(0, 'プログラミング', 0);
 INSERT INTO Category VALUES(0, '食事', 0);
 INSERT INTO Category VALUES(0, '読書', 0);
 INSERT INTO Category VALUES(0, 'ゲーム', 0);
--- mysql/DB/init/init_subCategory.sql
+++ mysql/DB/init/init_subCategory.sql
@@ -1,11 +1,11 @@
 DROP TABLE IF EXISTS SubCategory;
 CREATE TABLE SubCategory;
 
-INSERT INTO SubCategory VALUES(0, 'web', 1, 0);
-INSERT INTO SubCategory VALUES(0, 'game', 1, 0);
-INSERT INTO SubCategory VALUES(0, 'bot', 1, 0);
-INSERT INTO SubCategory VALUES(0, '哲学・宗教・社会', 2, 0);
-INSERT INTO SubCategory VALUES(0, '人生', 2, 0);
+INSERT INTO SubCategory VALUES(0, '哲学・宗教・社会', 1, 0);
+INSERT INTO SubCategory VALUES(0, '人生', 1, 0);
+INSERT INTO SubCategory VALUES(0, 'web', 2, 0);
+INSERT INTO SubCategory VALUES(0, 'game', 2, 0);
+INSERT INTO SubCategory VALUES(0, 'bot', 2, 0);
 INSERT INTO SubCategory VALUES(0, '料理', 3, 0);
 INSERT INTO SubCategory VALUES(0, '外食', 3, 0);
 INSERT INTO SubCategory VALUES(0, '勉強', 4, 0);
