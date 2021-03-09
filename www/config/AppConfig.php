48fac6a36c91292da7b8ff0aa52b23811856a361
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-03-10 01:24:10 +0900

    fix: Appconfig セミコロン

--- www/config/AppConfig.php
+++ www/config/AppConfig.php
@@ -5,6 +5,6 @@ namespace myapp\config;
 class AppConfig
 {
     const ARTCL_NUM = 9; //1ページあたりの記事数
-    const BY_ARTCL_NUM = 20 //BYページの1ページあたりの記事数
+    const BY_ARTCL_NUM = 20; //BYページの1ページあたりの記事数
     
 }
